374d54a2a78e76658cd9a7163fbdb6854228d650
duyykhuog <khuong862k4@gmail.com>
2025-06-10 21:55:33 +0700

    herbazen

---
+++ be/habalTea/HerbalTea/src/main/java/com/example/herbaltea/model/Product.java
@@ -0,0 +1,48 @@
+package com.example.herbaltea.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "ingredient")
+    private String ingredient;
+
+    @Column(name = "uses")
+    private String uses;
+
+    @Lob
+    @Column(name = "user_manual")
+    private String use;
+}
